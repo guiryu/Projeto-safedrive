95d947f7d6de21873b6052bffe40b607290d1e9c
Guilherme Ojima <138497326+guiryu@users.noreply.github.com>
2026-09-16 13:56:15 +0000

    correcao de muitos bugs

--- main.c
+++ main.c
@@ -12,11 +12,11 @@ Raphael
 
 
 void inicializar_matriz(int quantidade_linhas, int tamanho, float matriz[][tamanho]);
-void relatorio(int quantidade_linhas, float velocidades[][], float sensores_frontais[][] ,float sensores_laterais[][], float processamento[][] , int status[][]);
-void fusao_sensores(int linhas, int tamanho, float matriz[linhas][tamanho], int processamento[MAX_AMOSTRAS][2]);
+ void relatorio (int quantidade_linhas, float velocidades[MAX_AMOSTRAS][2], float sensores_frontais[MAX_AMOSTRAS][3] ,float sensores_laterais[MAX_AMOSTRAS][2], float processamento[MAX_AMOSTRAS][2] , float status[MAX_AMOSTRAS][3]);
+void fusao_sensores(int linhas, int tamanho, float matriz[linhas][tamanho], float processamento[MAX_AMOSTRAS][2]);
 void distancia_segura(int sensi, int atrito, int colunas, int quantidade, float vel[][colunas], float processamento[][colunas]);
-void analise_risco_frontal(int quantidade_linhas, float velocidades[][], float processamento[][], int status[][]);
-void assistente_faixa(int quantidade_linhas, float velocidades[][2], float sensores[][2], int status[][3]);
+void analise_risco_frontal(int quantidade_linhas, float velocidades[MAX_AMOSTRAS][2], float processamento[MAX_AMOSTRAS][2], float status[MAX_AMOSTRAS][3]);
+void assistente_faixa(int quantidade_linhas, float velocidades[][2], float sensores[][2], float status[][3]);
 
 int main(){
 
@@ -28,7 +28,7 @@ int main(){
     scanf("%d", &sensibilidade);
     
     float velocidades[MAX_AMOSTRAS][2], sensores_frontais[MAX_AMOSTRAS][3], sensores_laterais[MAX_AMOSTRAS][2], processamento[MAX_AMOSTRAS][2];
-    int status[MAX_AMOSTRAS][3];
+    float status[MAX_AMOSTRAS][3];
 
     // Menu
     do{
@@ -44,6 +44,7 @@ int main(){
         printf(SEPARADOR);
 
         if(menu==1){ // Inicializar matrizes
+            quantidade_linhas = 50;
             inicializar_matriz(quantidade_linhas, 2, velocidades);
             inicializar_matriz(quantidade_linhas, 3, sensores_frontais);
             inicializar_matriz(quantidade_linhas, 2, sensores_laterais);
@@ -66,9 +67,9 @@ int main(){
             scanf("%f", &sensores_frontais[quantidade_linhas][2]);
             // Sensores laterais
             printf("Distância da faixa esquerda: ");
-            scanf("%f", sensores_laterais[quantidade_linhas][0]);
+            scanf("%f", &sensores_laterais[quantidade_linhas][0]);
             printf("Distância da faixa direita: ");
-            scanf("%f", sensores_laterais[quantidade_linhas][1]);
+            scanf("%f", &sensores_laterais[quantidade_linhas][1]);
         }
         else if(menu==3){ // Processar e exibir relatorio
             
@@ -100,7 +101,7 @@ void inicializar_matriz(int quantidade_linhas, int colunas, float matriz[][colun
     }
 }
 
-void fusao_sensores(int linhas, int tamanho, float matriz[linhas][tamanho], int processamento[MAX_AMOSTRAS][2]){
+void fusao_sensores(int linhas, int tamanho, float matriz[linhas][tamanho], float processamento[MAX_AMOSTRAS][2]){
     float auxiliar=0;
     //ORDENAÇÂO
     for (int i=0; i<linhas; i++){
@@ -140,7 +141,7 @@ void distancia_segura(int sensi, int atrito, int colunas, int quantidade, float 
          
 }
 
-    void analise_risco_frontal(int quantidade_linhas, float velocidades[][2], float processamento[][3], int status[][3]){
+    void analise_risco_frontal(int quantidade_linhas, float velocidades[MAX_AMOSTRAS][2], float processamento[MAX_AMOSTRAS][2], float status[MAX_AMOSTRAS][3]){
         float distancia_validada = processamento[1][0];
         float distancia_segura = processamento[1][1];
         float velocidadeAtual =velocidades[1][0];
@@ -170,7 +171,7 @@ void distancia_segura(int sensi, int atrito, int colunas, int quantidade, float 
         }
     }
 
-void assistente_faixa(int quantidade_linhas, float velocidades[][2], float sensores[][2], int status[][3]){
+void assistente_faixa(int quantidade_linhas, float velocidades[][2], float sensores[][2], float status[][3]){
     float margem_dinamica;
     for (int i=0; i<quantidade_linhas; i++){
         margem_dinamica = (velocidades[i][0] - 80) * 0.01 + 0.5;
@@ -201,27 +202,27 @@ void assistente_faixa(int quantidade_linhas, float velocidades[][2], float senso
 
 
 
-    void relatorio (int quantidade_linhas, float velocidades[MAX_AMOSTRAS][2], float sensores_frontais[MAX_AMOSTRAS][3] ,float sensores_laterais[MAX_AMOSTRAS][2], float processamento[MAX_AMOSTRAS][2] , int status[MAX_AMOSTRAS][3]){
+    void relatorio (int quantidade_linhas, float velocidades[MAX_AMOSTRAS][2], float sensores_frontais[MAX_AMOSTRAS][3] ,float sensores_laterais[MAX_AMOSTRAS][2], float processamento[MAX_AMOSTRAS][2] , float status[MAX_AMOSTRAS][3]){
 
         for (int i=0; i<quantidade_linhas; i++){
             printf(SEPARADOR);
             printf("Amostra %d\n", i+1);
             printf("DADOS DE ENTRADA:\n");
             printf("=====VELOCIDADES=====\n");
-            printf("Velocidade Atual: %.2f\n", &velocidades[i][0]);
-            printf("Velocidade do veículo à frente: %.2f\n", &velocidades[i][1]);
+            printf("Velocidade Atual: %.2f\n", velocidades[i][0]);
+            printf("Velocidade do veículo à frente: %.2f\n", velocidades[i][1]);
             printf("=====SENSORES FRONTAIS=====\n");
-            printf("Sensor Radar: %.2f\n", &sensores_frontais[i][0]);
-            printf("Sensor Lidar: %.2f\n", &sensores_frontais[i][1]);
-            printf("Sensor Câmera: %.2f\n", &sensores_frontais[i][2]);
+            printf("Sensor Radar: %.2f\n", sensores_frontais[i][0]);
+            printf("Sensor Lidar: %.2f\n", sensores_frontais[i][1]);
+            printf("Sensor Câmera: %.2f\n", sensores_frontais[i][2]);
             printf("=====SENSORES LATERAIS=====\n");
-            printf("Distância da faixa esquerda: %.2f\n", &sensores_laterais[i][0]);
-            printf("Distância da faixa direita: %.2f\n", &sensores_laterais[i][1]);
+            printf("Distância da faixa esquerda: %.2f\n", sensores_laterais[i][0]);
+            printf("Distância da faixa direita: %.2f\n", sensores_laterais[i][1]);
             
             printf(SEPARADOR);
             printf("DADOS PROCESSADOS:\n ");
-            printf("Distância Validada: %.2f\n", &processamento[i][0]);
-            printf("Distância Segura Exigida: %.2f\n", &processamento[i][1]);
+            printf("Distância Validada: %.2f\n", processamento[i][0]);
+            printf("Distância Segura Exigida: %.2f\n", processamento[i][1]);
             
             printf(SEPARADOR);
             printf("TRADUTOR DE STATUS E MENSAGENS DE ALERTA\n");
@@ -255,11 +256,11 @@ void assistente_faixa(int quantidade_linhas, float velocidades[][2], float senso
             printf("DECISÃO GERAL DO SISTEMA\n");
         
             if (status[i][0] ==2 || status[i][1] ==2 || status[i][2]==2){
-                printf("STATUS GERAL: INTERVEÇÃO CRÍTICA EXIGIDA");
+                printf("STATUS GERAL: INTERVEÇÃO CRÍTICA EXIGIDA\n");
             }else if(status[i][0] ==1 || status[i][1] ==1 || status[i][2]==1){
-                printf("STATUS GERAL: ATENÇÃO");
+                printf("STATUS GERAL: ATENÇÃO\n");
             }else{
-                printf("STATUS GERAL: NORMAL");
+                printf("STATUS GERAL: NORMAL\n");
             }
         }
 
